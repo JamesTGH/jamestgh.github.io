0d7572c6927cefe5c20b5dfc47952c62807e654c
JamesTGH <26239426+JamesTGH@users.noreply.github.com>
2023-12-08 18:16:35 -0600

    Updated js/cinemagnet.js

--- js/cinemagnet.js
+++ js/cinemagnet.js
@@ -38,43 +38,88 @@ async function getMagnetStream(debridAgent, debridKey, magnetHash) {
 	}
 }
 
+// async function runFunction() {
+// 	const debridKey = $('#debridkey').val();
+// 	const magnetHash = $('#magnethash').val();
+	
+// 	if (debridKey && magnetHash) {
+// 		if (/^[0-9a-f]{40}$/i.test(magnetHash)) {
+// 			const magnetStreamUrl = await getMagnetStream('cinemagnet', debridKey, magnetHash);
+// 			$('#magnetstreamurl').attr('href', magnetStreamUrl);
+// 			$('#magnetstreamvideo').attr('src', magnetStreamUrl).get(0).load();
+// 			$('#magnetstreamvideo').get(0).play();
+// 			var video = $('#magnetstreamvideo')[0]; // Get the video element as a plain DOM object
+
+// 			if (video.canPlayType) {
+// 				var codecs = video.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
+// 				console.log('Supported video codecs: ' + codecs);
+		
+// 				var videoUrl = video.currentSrc;
+// 				console.log('Video URL: ' + videoUrl);
+		
+// 				var videoTrack = video.videoTracks[0];
+// 				if (videoTrack) {
+// 					console.log('Video Track Information:');
+// 					console.log('Track ID: ' + videoTrack.id);
+// 					console.log('Kind: ' + videoTrack.kind);
+// 					console.log('Label: ' + videoTrack.label);
+// 				}
+		
+// 				var audioTrack = video.audioTracks[0];
+// 				if (audioTrack) {
+// 					console.log('Audio Track Information:');
+// 					console.log('Track ID: ' + audioTrack.id);
+// 					console.log('Kind: ' + audioTrack.kind);
+// 					console.log('Label: ' + audioTrack.label);
+// 				}
+// 			} else {
+// 				console.error('Video element is not supported in this browser.');
+// 			}
+// 		}
+// 	}
+// }
+
 async function runFunction() {
 	const debridKey = $('#debridkey').val();
 	const magnetHash = $('#magnethash').val();
-	
+
 	if (debridKey && magnetHash) {
 		if (/^[0-9a-f]{40}$/i.test(magnetHash)) {
 			const magnetStreamUrl = await getMagnetStream('cinemagnet', debridKey, magnetHash);
 			$('#magnetstreamurl').attr('href', magnetStreamUrl);
-			$('#magnetstreamvideo').attr('src', magnetStreamUrl).get(0).load();
-			$('#magnetstreamvideo').get(0).play();
-			var video = $('#magnetstreamvideo')[0]; // Get the video element as a plain DOM object
+			$('#magnetstreamvideo').attr('src', magnetStreamUrl);
 
-			if (video.canPlayType) {
-				var codecs = video.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
-				console.log('Supported video codecs: ' + codecs);
-		
-				var videoUrl = video.currentSrc;
-				console.log('Video URL: ' + videoUrl);
-		
-				var videoTrack = video.videoTracks[0];
-				if (videoTrack) {
-					console.log('Video Track Information:');
-					console.log('Track ID: ' + videoTrack.id);
-					console.log('Kind: ' + videoTrack.kind);
-					console.log('Label: ' + videoTrack.label);
+			$('#magnetstreamvideo').on('loadedmetadata', function() {
+				var video = $('#magnetstreamvideo')[0];
+
+				if (video.canPlayType) {
+					var codecs = video.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
+					console.log('Supported video codecs: ' + codecs);
+
+					var videoUrl = video.currentSrc;
+					console.log('Video URL: ' + videoUrl);
+
+					var videoTrack = video.videoTracks[0];
+					if (videoTrack) {
+						console.log('Video Track Information:');
+						console.log('Track ID: ' + videoTrack.id);
+						console.log('Kind: ' + videoTrack.kind);
+						console.log('Label: ' + videoTrack.label);
+					}
+
+					var audioTrack = video.audioTracks[0];
+					if (audioTrack) {
+						console.log('Audio Track Information:');
+						console.log('Track ID: ' + audioTrack.id);
+						console.log('Kind: ' + audioTrack.kind);
+						console.log('Label: ' + audioTrack.label);
+					}
+				} else {
+					console.error('Video element is not supported in this browser.');
 				}
-		
-				var audioTrack = video.audioTracks[0];
-				if (audioTrack) {
-					console.log('Audio Track Information:');
-					console.log('Track ID: ' + audioTrack.id);
-					console.log('Kind: ' + audioTrack.kind);
-					console.log('Label: ' + audioTrack.label);
-				}
-			} else {
-				console.error('Video element is not supported in this browser.');
-			}
+			});
+
+			$('#magnetstreamvideo').get(0).load();
 		}
 	}
 }
